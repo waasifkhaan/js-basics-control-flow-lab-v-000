13e566cafc77c58bb40ed013840119adc7705524
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 03:22:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (num <= 400) {
 else if (num > 2000) {
   message = 'I will gladly take your thirty bucks.';
 }
-else if (num > 2500) {
+else if (num >= 2500) {
   message = 'No can do.';
 }
 return message;
