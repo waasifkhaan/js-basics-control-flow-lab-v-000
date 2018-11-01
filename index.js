01699e3d351ae077e803ae0a6f6fef3e58889199
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 03:09:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // Write your code in this file!
-function scuberGreetingForFeet() {
+function scuberGreetingForFeet(num) {
+let message; 
+if (num < 400) {
+  'This one is on me!';
+} 
 
+}
+function addFive(someNumber) {
+  //everything I want my function to do I put inside these curly braces
+  //in this example, lets say I want my function, addFive, to add 5 to any number I pass in (someNumber), but only IF the number is greater than zero
+  let result
+  if (someNumber > 0) {
+    result = someNumber + 5;
+  }
+  //at the end, if I want my function to return something, I need to state it:
+  return result
 }
