294514cbbc689c484ce96e4ef8f455e73816a27c
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 03:41:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num) {
+  let message;
+    if (num <= 400) {
+      message = 'This one is on me!';
+    }
+    else if (1999 < num && num < 2500) {
+      message = 'I will gladly take your thirty bucks.';
+    }
+    else {
+      message = 'No can do.';
+    }
+  return message;
+}
+
+function ternaryCheckCity(city){
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+
+      case 'not as generous':
+        return'Thank you.';
+
+      default:
+        return 'Bye.';
+
+  }
+}
