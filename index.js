1c543bec7ee04436e27f2287b5e89ed648447ed1
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 03:30:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // Write your code in this file!
 function scuberGreetingForFeet(num) {
-let message;
-if (num <= 400) {
-  message = 'This one is on me!';
-}
-else if (1999 < num && num < 2500) {
-  message = 'I will gladly take your thirty bucks.';
-}
-else {
-  message = 'No can do.';
-}
-return message;
-
+  let message;
+    if (num <= 400) {
+      message = 'This one is on me!';
+    }
+    else if (1999 < num && num < 2500) {
+      message = 'I will gladly take your thirty bucks.';  
+    }
+    else {
+      message = 'No can do.';
+    }
+  return message;
 }
 
 function ternaryCheckCity(city){
