a659f9848d36acb7b22618453e5dabbb97fa0685
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 03:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,3 @@ if (num < 400) {
 returen message;
 
 }
-function addFive(someNumber) {
-  //everything I want my function to do I put inside these curly braces
-  //in this example, lets say I want my function, addFive, to add 5 to any number I pass in (someNumber), but only IF the number is greater than zero
-  let result
-  if (someNumber > 0) {
-    result = someNumber + 5;
-  }
-  //at the end, if I want my function to return something, I need to state it:
-  return result
-}
