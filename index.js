05e02cbd754a40d944cb43af9410d14fecdbdb11
Wasif Khan <waasif.khaan@gmail.com>
2018-11-01 03:37:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet(num) {
       message = 'This one is on me!';
     }
     else if (1999 < num && num < 2500) {
-      message = 'I will gladly take your thirty bucks.';  
+      message = 'I will gladly take your thirty bucks.';
     }
     else {
       message = 'No can do.';
@@ -16,4 +16,21 @@ function scuberGreetingForFeet(num) {
 function ternaryCheckCity(city){
 
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(tip){
+ 
+let note;
+ 
+switch (tip) {
+    case 'generous':
+        note = 'Thank you so much.';
+        break;
+    case 'not as generous':
+        note = 'Thank you.';
+        break;
+    case 'thanks for everything':
+        note = 'Bye';
+        break;
+}
 }
